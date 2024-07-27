734a5edd20368605b122ee850b3523cf9e2dea23
Glowja <gloryjibril433@gmail.com>
2024-07-27 20:14:11 +0100

    getElementById addition

--- script.js
+++ script.js
@@ -5,5 +5,13 @@ document.addEventListener('DOMContentLoaded',function(){
         event.preventDefault()
     })
  const feedbackDiv=document.getElementById('form-feedback')
+ let username=document.getElementById('username');
+ let usernamevalue = username.value;
+
+ let email=document.getElementById('email');
+ let emailvalue = email.value;
+
+ let password=document.getElementById('password');
+ let passwordvalue = password.value;
 })
  
