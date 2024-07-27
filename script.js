84d3b6135b7e557af2be13e329e6e8101e39c659
Glowja <gloryjibril433@gmail.com>
2024-07-27 20:45:23 +0100

    trim added

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 document.addEventListener('DOMContentLoaded',function(){
 
-    const form=document.getElementById('registration-form')
+    const form = document.getElementById('registration-form')
     document.addEventListener("submit", () =>{
         event.preventDefault()
     })
- const feedbackDiv=document.getElementById('form-feedback')
- let username=document.getElementById('username');
+ const feedbackDiv = document.getElementById('form-feedback')
+ let username = document.getElementById('username');
  let usernamevalue = username.value;
+ username = username.trim();
 
- let email=document.getElementById('email');
+ let email = document.getElementById('email');
  let emailvalue = email.value;
-
- let password=document.getElementById('password');
+ email = email.trim();
+ 
+ let password = document.getElementById('password');
  let passwordvalue = password.value;
+ password = password.trim(); 
 })
  
