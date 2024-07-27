a20b8a95cbcd8d3fca04cde4ca804fd4820a91a5
Glowja <gloryjibril433@gmail.com>
2024-07-27 19:13:04 +0100

    wrap

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.addEventListener('RegistrationForm',function(){
 
-})
- const form=document.getElementById('registration-form')
+    const form=document.getElementById('registration-form')
     document.addEventListener("submit", () =>{
         event.preventDefault()
     })
  const feedbackDiv=document.getElementById('form-feedback')
+})
+ 
