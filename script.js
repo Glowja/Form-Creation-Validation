f136a1dc6993836eaf85c59dd0c5091be6715f46
Glowja <gloryjibril433@gmail.com>
2024-07-27 19:11:26 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('RegistrationForm',function(){
+
+})
+ const form=document.getElementById('registration-form')
+    document.addEventListener("submit", () =>{
+        event.preventDefault()
+    })
+ const feedbackDiv=document.getElementById('form-feedback')
