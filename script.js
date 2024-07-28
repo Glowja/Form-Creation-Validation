5023bbf97ba2ca36730671a9123ae69f07fd6d6d
Glowja <gloryjibril433@gmail.com>
2024-07-28 18:42:49 +0100

    commit

--- script.js
+++ script.js
@@ -3,32 +3,52 @@ document.addEventListener('DOMContentLoaded',function(){
     const form = document.getElementById('registration-form')
     document.addEventListener("submit", () =>{
         event.preventDefault()
-    })
- const feedbackDiv = document.getElementById('form-feedback')
- let username = document.getElementById('username');
- let usernamevalue = username.value;
- username = username.trim();
- console.log(username)
+        const feedbackDiv = document.getElementById('form-feedback')
+        let username = document.getElementById('username');
+        let usernamevalue = username.value.trim();
+        console.log(username)
 
- let email = document.getElementById('email');
- let emailvalue = email.value;
- email = email.trim();
+        let email = document.getElementById('email');
+        let emailvalue = email.value.trim();
+        
+        let password = document.getElementById('password');
+        let passwordvalue = password.value.trim();
+
+        let isValid = true
+ //let messages = [username.error(username, $[must], $[longer])]
+        let messages = [];
+        if (username.length < 3) {
+            isValid = false 
+            messages.push('password too short');
+          }else{
+          isValid = true
+        }
+        if (email.includes('@') && email.includes('.')) {
+          isValid = true
+          }else{
+            isValid = false
+            messages.push("email must include '@' and '.'")
+          }
+
+        if (password >= 8) {
+          isValid = true
+          }else{
+            isValid = false
+            messages.push('password must be not be less than 8 characters')
+          }
+        if (isValid === true) {
+          feedbackDiv.style.color = '#28a745'
+          feedbackDiv.innerHTML = 'registeration is sucessful'
+          }else{
+            feedbackDiv.innerHTML = messages
+          }
+    })
  
- let password = document.getElementById('password');
- let passwordvalue = password.value;
- password = password.trim(); 
 
  //condition statement
- let isValid = true
- if (username.length < 3) {
-    isValid = false 
-  }
-  if (email) {
-    isValid = false
-  } 
+ 
 
- let messages = [username.error(username, $[must], $[longer])
-]
+ 
  console.log(messages)
  //messages.push()
 })
